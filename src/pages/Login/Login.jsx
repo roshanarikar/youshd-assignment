437c34338675a926a382fc2b86753895bbcda51c
Roshan Arikar <85448130+roshanarikar@users.noreply.github.com>
2023-01-21 10:40:57 +0530

    login page in progress

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,32 @@
-
+import  { useState } from 'react'
+import PhoneInput from 'react-phone-number-input'
+import "./Login.css"
+import 'react-phone-number-input/style.css'
 
 export const Login = () => {
+    const [value, setValue] = useState()
+
     return(
-        <div>Login</div>
+        <div id="HomeCont">
+            <div>
+                <p>please enter your phone number to get started</p>
+            </div>
+            <div>
+                <div>
+                <PhoneInput
+                    international
+                    defaultCountry="US"
+                    value={value}
+                    onChange={setValue}/>
+                </div>
+            </div>
+            <div className='space'></div>
+            <div>
+                <div>
+                    <div><button>+</button></div>
+                    <div><button>Continue</button></div>
+                </div>
+            </div>
+        </div>
     )
 }
